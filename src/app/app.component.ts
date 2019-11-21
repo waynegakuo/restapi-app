467563b9b234d45f241562ba713de12c5c0ab335
waynegakuo <waynejotham@gmail.com>
2019-11-21 20:07:56 +0300

    REST API for starters initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService} from './services/user.service';
+import {User} from './model/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'restapi-app';
+  name = 'by Wayne Gakuo';
+  users: User[];
+
+  constructor( private service: UserService) {}
+  ngOnInit() {
+    return this.service.getUsers().subscribe(data => { this.users = data; });
+}
 }
